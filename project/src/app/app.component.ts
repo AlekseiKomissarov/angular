0cb5e3092ae7c78dd0019d0568e6250baba4218e
ALEKSEI <aleksei@MacBook-Pro-ALEKSEI.local>
2019-09-04 16:26:59 +0300

    rxjs interval

--- project/src/app/app.component.ts
+++ project/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import {Component} from '@angular/core';
+import {interval, Subscription} from 'rxjs';
+import {map, filter, switchMap} from 'rxjs/operators';
 
 
 @Component({
@@ -10,5 +12,25 @@ import {Component} from '@angular/core';
 
 export class AppComponent{
 
+  sub: Subscription
+
+  constructor(){
+
+    const intervalSteam$ = interval(1000)
+
+    this.sub = intervalSteam$
+      .pipe(
+        filter(value => value%2===0),
+        map((value) => `Mappped value ${value}`),
+        switchMap(()=>interval(500))
+      )
+      .subscribe((value) => {
+        console.log(value)
+      })
+  }
+
+  stop(){
+    this.sub.unsubscribe()
+  }
 
 }
