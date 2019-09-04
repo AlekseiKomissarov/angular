e9e240812b89416224f4439d86203888925fc21f
ALEKSEI <aleksei@MacBook-Pro-ALEKSEI.local>
2019-09-04 14:41:02 +0300

    clear project for rxjx

--- project/src/app/app.component.ts
+++ project/src/app/app.component.ts
@@ -1,10 +1,5 @@
 import {Component} from '@angular/core';
-import {Observable} from 'rxjs';
 
-export interface Post {
-  title: string
-  text: string
-}
 
 @Component({
   selector: 'app-root',
@@ -14,40 +9,6 @@ export interface Post {
 
 
 export class AppComponent{
-  // isVisible = true
-  // title = 'project'
 
-  // e: number = Math.E
-  // str = 'hello world!'
-
-  search = ''
-  searchField = 'title'
-
-  p: Promise<string> = new Promise<string>(resolve => {
-    setTimeout(()=>{
-      resolve('Promise resolve')
-    }, 4000)
-  })
-
-  date: Observable = new Observable(obs=>{
-    setInterval(()=>{
-      obs.next(new Date())
-    }, 1000)
-  })
-
-
-
-  posts: Post[] = [
-    {title: 'Beer', text: 'the best beer in the world!'},
-    {title: 'Bread', text: 'the best bread in the world!'},
-    {title: 'Language', text: 'the best language in the world!'}
-  ]
-
-  addPost(){
-    this.posts.unshift({
-      title: 'Angular 8',
-      text: 'new post about angular'
-    })
-  }
 
 }
