d136d2304665f8a208d4d5b05227fe61b7eae233
ALEKSEI <aleksei@MacBook-Pro-ALEKSEI.local>
2019-09-05 11:06:31 +0300

    formsAndValidations

--- project/src/app/app.component.ts
+++ project/src/app/app.component.ts
@@ -1,14 +1,40 @@
-import {Component} from '@angular/core';
-
+import {Component, OnInit} from '@angular/core'
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+export class AppComponent implements OnInit{
+  form: FormGroup
 
+  ngOnInit() {
+    this.form = new FormGroup({
+      email: new FormControl('', [
+        Validators.email,
+        Validators.required
+      ]),
+      password: new FormControl(null,[
+        Validators.required,
+        Validators.minLength(6)
+      ]),
 
-export class AppComponent{
+      address: new FormGroup( {
+        country: new FormControl('ru'),
+        city: new FormControl('', Validators.required)
+      })
+    })
+  }
 
+  submit(){
+    if (this.form.valid ){
+      console.log('Form submitted', this.form)
+      const formData = {...this.form.value}
+      console.log('Form Data ', formData)
+    }
+
+  }
 
 }
+
--- project/src/app/app.module.ts
+++ project/src/app/app.module.ts
@@ -1,18 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser'
+import {NgModule} from '@angular/core'
 
-import { AppComponent } from './app.component';
-import {FormsModule} from '@angular/forms';
+import {AppComponent} from './app.component'
+import {FormsModule, ReactiveFormsModule} from '@angular/forms'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [
+
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
