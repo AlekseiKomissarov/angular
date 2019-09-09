3c8b8f2f1fab58a3ec91f0165a36e3287c6e984b
ALEKSEI <aleksei@MacBook-Pro-ALEKSEI.local>
2019-09-09 16:19:06 +0300

    dinamicComponents

--- project/src/app/app-routing.module.ts
+++ project/src/app/app-routing.module.ts
@@ -10,7 +10,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes,{
-    preloadingStrategy: PreloadAllModules // выполняет загрузку остальных модулей после загрузки всех скриптов
+    preloadingStrategy: PreloadAllModules // выполняет загрузку остальных модулей после загрузки всех scripts
   })],
   exports: [RouterModule]
 })
