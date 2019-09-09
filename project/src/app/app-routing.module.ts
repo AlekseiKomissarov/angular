2dea6f0d36754c09500e5c775d993082a2b57cae
ALEKSEI <aleksei@MacBook-Pro-ALEKSEI.local>
2019-09-09 13:53:41 +0300

    modules

--- project/src/app/app-routing.module.ts
+++ project/src/app/app-routing.module.ts
@@ -1,30 +1,19 @@
-import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
-import {HomeComponent} from './home/home.component';
-import {AboutComponent} from './about/about.component';
-import {PostsComponent} from './posts/posts.component';
-import {PostComponent} from './post/post.component';
-import {AboutExtraComponent} from './about-extra/about-extra.component';
-import {ErrorPageComponent} from './error-page/error-page.component';
-import {AuthGuard} from './auth.guard';
+import {NgModule} from '@angular/core'
+import {PreloadAllModules, RouterModule, Routes} from '@angular/router'
+import {HomePageComponent} from './home-page/home-page.component'
+
 
 const routes: Routes = [
-  {path:'', component: HomeComponent},
-  {path:'about', component: AboutComponent, canActivateChild: [AuthGuard], children: [
-    {path: 'extra', component: AboutExtraComponent}
-    ]},
-  {path:'posts', component: PostsComponent, canActivate:[AuthGuard]},
-  {path:'posts/:id', component: PostComponent},
-  {path:'error', component: ErrorPageComponent},
-  {path:'**', redirectTo: '/error'}
+  {path: '', component: HomePageComponent, pathMatch: 'full'},
+  {path: 'about', loadChildren: './about-page/about-page.module#AboutPageModule'}
 ]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,{
+    preloadingStrategy: PreloadAllModules // выполняет загрузку остальных модулей после загрузки всех скриптов
+  })],
   exports: [RouterModule]
 })
-export class AppRoutingModule{
-
-
+export class AppRoutingModule {
 
 }
